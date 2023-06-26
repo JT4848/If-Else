a16eca13d00f04f0d2687f7cb151d22974d5735d
JT4848 <jtpricone@gmail.com>
2023-06-26 08:19:33 -0400

    truthy section done

---
+++ truthy.js
@@ -0,0 +1,37 @@
+
+//Make a input value
+//Assign correct input value for each if statment
+//Run each if statement individually to make sure im seeing the expected output
+
+
+
+const input = "";
+
+// if(input === `I am a string`){
+//   console.log(true);
+// }
+
+// if(input === false){
+//   console.log("The boolean value false is falsy");
+// }
+
+
+// if(input === null){
+//   console.log("The null value is falsy");
+// }
+
+
+
+// if(input === undefined){
+//   console.log("undefined is falsy");
+// }
+
+
+// if(input === 0){
+//   console.log("The number 0 is falsy");
+// }
+
+if(input === ""){
+  console.log("The empty string is falsy");
+}
+
