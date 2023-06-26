64cb9e567bff59c6097acba30272b43c394f389f
JT4848 <jtpricone@gmail.com>
2023-06-26 09:06:48 -0400

    finsished greater than 5 portion

---
+++ Gr8than5.js
@@ -0,0 +1,36 @@
+//Make two number variables 
+//Assign correct inputs for each value
+//Go through each input and make sure I see correct output
+
+
+
+
+
+// const num1 = 5;
+// const num2 = 6;
+
+// const num1 = 10;
+// const num2 = 11;
+
+// const num1 = 0;
+// const num2 = 0;
+
+// const num1 = 1000;
+// const num2 = -1000;
+
+// const num1 = 6;
+// const num2 = 4;
+
+const num1 = 5;
+const num2 = 5;
+
+
+if(num1 && num2 >= 5){
+  console.log(true);
+}
+else{
+  console.log(false);
+}
+
+
+
