9f48cae015a51feae2d0880bb5a82ea1a73f11b6
Tom Carfano <tomcarfano@TC-Macbook-Pro.lan>
2023-06-28 12:17:12 -0700

    1

---
+++ 6-28/codeStretch.js
@@ -0,0 +1,14 @@
+let bunnyCount = 50;
+
+let additionalBunnies = 100;
+
+let totalBunnies = bunnyCount + additionalBunnies;
+
+console.log(totalBunnies);
+
+let firstName = "Tom";
+
+let lastName = "Carfano";
+
+let fullName = firstName + " " + lastName;
+
---
+++ guidedPractice/index.js
@@ -0,0 +1,50 @@
+//Create input value
+const zero = 0;
+//Check if 0 is even
+if(zero % 2 === 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
+//Check if 1 is Odd
+let one = 1;
+if(one % 2 === 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
+//Check if 5 is Odd
+let five = 5;
+if(five % 2 === 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
+// Check if 16 is Even
+let six = 16;
+if(six % 2 === 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
+// Check if 256 is Even
+let twoFive = 256;
+if(twoFive % 2 === 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
+// Check if -5 is Odd
+let negativeFive = -5;
+if(negativeFive % 2 === 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
+//Check if -5555555555555 is Odd
+let negative = -5555555555555;
+if(negative % 2 === 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
